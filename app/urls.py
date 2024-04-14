a6370b5b596e6bbbfa54dd90867c2ff9ce681341
AnjasFedo <fedoaf98@gmail.com>
2024-04-14 15:29:53 +0700

    Url Patterns

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
-    path('incoming-mail', views.incoming_mail, name='incoming_mail')
+    path('incoming-mail', views.incoming_mail, name='incoming_mail'),
+    path('outgoing-mail', views.outgoing_mail, name='outgoing_mail'),
+    path('incoming-disposition', views.incoming_disposition,
+         name='incoming_disposition'),
+    path('incoming-disposition', views.incoming_disposition,
+         name='incoming_disposition'),
+    path('agenda', views.agenda, name='agenda'),
 ]
