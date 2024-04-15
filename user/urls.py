a3a9ef31840e2a613da58baf0610b7a35313dfe3
AnjasFedo <fedoaf98@gmail.com>
2024-04-16 00:17:09 +0700

    Reset Password Process

--- user/urls.py
+++ user/urls.py
@@ -7,11 +7,11 @@ urlpatterns = [
     path('logout/', views.user_logout, name='user_logout'),
     
     path('reset-password/', auth_views.PasswordResetView.as_view(
-        template_name='password_reset.html'), name='reset_password'),
+        template_name='reset_password.html'), name='reset_password'),
     path('reset-password-done/', auth_views.PasswordResetDoneView.as_view(
-        template_name='password_reset_done.html'), name='password_reset_done'),
+        template_name='reset_password_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(
-        template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+        template_name='reset_password_confirm.html'), name='password_reset_confirm'),
     path('reset-password-complete/', auth_views.PasswordResetCompleteView.as_view(
-        template_name='password_reset_complete.html'), name='password_reset_complete'),
+        template_name='reset_password_complete.html'), name='password_reset_complete'),
 ]
